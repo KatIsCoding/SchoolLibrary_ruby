31cdce8939c55e0a87fe08550955c69f754dc42d
KatIsCoding <fabrigoru@gmail.com>
2021-12-22 14:47:02 -0600

    Implemented corrector inside the person class

--- person.rb
+++ person.rb
@@ -1,13 +1,20 @@
+require_relative './corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   attr_reader :id
   attr_accessor :name, :age
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
 
   def can_use_services?
     is_of_age? || @parent_permission
