57ef0f0283035e6994914046f56db748ddee4ed8
KatIsCoding <fabrigoru@gmail.com>
2021-12-23 15:35:39 -0600

    Add rental class with defined methods to add a book or owner

---
+++ rental.rb
@@ -0,0 +1,16 @@
+class Rental
+
+  attr_accessor :date, :book, :owner
+  def initialize(date, book, owner)
+    @date = date
+    @book = book
+    @owner = owner
+  end
+  def book(book)
+    @book.add_rental(self)
+  end
+  def owner(owner)
+    @owner.add_rental(self)
+  end
+    
+end
